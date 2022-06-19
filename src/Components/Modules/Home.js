c70425b63cd3c674e057e6a6f11c898909285f01
Andy Caseres <andycaseres2@gmail.com>
2022-06-19 00:21:28 -0500

    implement modal

--- src/Components/Modules/Home.js
+++ src/Components/Modules/Home.js
@@ -1,17 +1,24 @@
-import store from '../../state/store';
+import { useState } from 'react'
+import store from '../../state/store'
 import * as actions from '../../state/actions/auth_actions'
+import { Modal } from '../Modal'
 
 const Home = () => {
+  const [modalOpen, setModalOpen] = useState(false)
 
-    return (
-        <div >
-            <h1>Home</h1>
-            <button onClick={() => store.dispatch(actions.login())}>AUTH</button>
-            <button onClick={() => store.dispatch(actions.prueba())}>PROBAR</button>
-            
-
-        </div>
-    )
+  return (
+    <div>
+      <h1>Home</h1>
+      <button onClick={() => store.dispatch(actions.login())}>AUTH</button>
+      <button onClick={() => store.dispatch(actions.prueba())}>PROBAR</button>
+      <button onClick={() => setModalOpen(true)}>OpenModal</button>
+      <Modal open={modalOpen} setOpen={setModalOpen}>
+        <h2>Eleazar</h2>
+        <label for="">Name</label>
+        <input placeholder="Name" type="text" />
+      </Modal>
+    </div>
+  )
 }
 
 export default Home
