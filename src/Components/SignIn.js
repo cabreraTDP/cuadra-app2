426309aa36a324fd32458bee73683e4404bb6e69
Eleazar Cabrera <eleazarcabrera@etechdatapeople.com>
2022-12-01 23:33:06 -0600

    login refactor

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,10 +1,9 @@
 import { useState } from 'react';
-import '../CSS/SignIn.css'
 import {useNavigate} from 'react-router-dom'
 import store from '../state/store';
 import {login}  from '../state/actions/auth_actions'
 import { Post } from '../utils/axiosUtils';
-
+import logo from '../svg/logo.svg';
 
 const SignIn = () => {
 
@@ -41,48 +40,47 @@ const SignIn = () => {
     };
 
     return (
-        <div id='principal'>
-            <div >
-            </div>
-            <div className='contenedor'>
-                <h1 style={{color:'blue'}}>Bienvenido(a) a <b style={{color:'black'}}>CUADRA</b> </h1>
-                <h2 style={{color:'blue'}}><b> Ingresa aquí</b></h2>
-                <form onSubmit={onSubmitHandler}>
-                <div id='inputs'>
-                    <label style={{fontSize:'23px'}}><b>Usuario</b></label >
-                    <input className='input' style={{padding: '5px'}} name='usuario' onChange={onChangeText} required/>
+        <div style={{position:'relative', width:'100%', height:'100%', backgroundColor:'#B9BBFA', opacity: 0.9}}>
+            <div style={{position:'absolute', top: '25%', left: '30%', width: '40%', height:'50%',margin: '-25px 0 0 -25px', border:'2px groove black', 
+                borderRadius: 50, backgroundColor:'RGBA(255,255,255,0.5)', boxShadow:'5px 5px 5px 5px gray'}}>
+                <div style={{marginLeft: '25%', width: '50%', marginTop: 20}}>
+                <img src={logo} ></img>
                 </div>
-                <div id='inputs' >
-                    <label style={{fontSize:'23px'}}><b>Contraseña</b></label >
-                    <input className='input' style={{padding: '5px'}} type='password' name='password' onChange={onChangeText} required/>
+                <form onSubmit={onSubmitHandler}>
+                    
+                <div style={{width:'60%', marginLeft:'20%', marginTop:25}}>
+                    <label style={{fontSize:'23px'}}>Usuario:</label >
+                    <input style={{padding: '5px', width:'100%', marginBottom:30}} name='usuario' onChange={onChangeText} required/>
 
-                    <a href="https://www.mozilla.org/es-ES/" id='a'>Olvidé mi contraseña</a>.
+                    <label style={{fontSize:'23px'}}>Contraseña:</label >
+                    <input className='input' style={{padding: '5px', width:'100%',marginBottom:5}} type='password' name='password' onChange={onChangeText} required/>
+                    
+                    <a href="" id='a'>Olvidé mi contraseña</a>.
                     <h3 style={{color:'red', textAlign:'center'}}>{error?error:null}</h3>
                 </div>
 
                 <div style={{
-                    marginTop: '10px',
-                    paddingLeft: '200px',
-
-                }}>
+                    marginTop: 30,
+                    paddingLeft: '200px',}}>
                      
                     <button type="submit"
                         style={{
-                            backgroundColor: 'blue',
                             border: 0,
+                            backgroundColor: '#1C70E6',
                             color: 'white',
                             height: '35px',
                             width: '170px',
                             borderRadius: '6px',
                             fontSize:'20px'
-                        }}
-
-                    >Iniciar Sesion
+                        }}>
+                        Iniciar Sesion
                     </button>
+                    
                 </div>
                 </form>
 
             </div>
+
             
         </div>
     )
