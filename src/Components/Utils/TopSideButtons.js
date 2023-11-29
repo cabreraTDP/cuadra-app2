37541a5ec59fcd98657640ce1af14e5b2ed0c556
eleazar cabrera <eleazarcabrera@tehdatapeople.com>
2023-11-29 15:23:54 -0600

    fix deps

--- src/Components/Utils/TopSideButtons.js
+++ src/Components/Utils/TopSideButtons.js
@@ -17,19 +17,24 @@ const TopSideButtons = ({removeFilter, applyFilter}) => {
         setFilterParam("")
     }
 
+    const aStyle = {
+        "text-decoration": "none",
+        "color": "inherit"
+    }
+
     return(
         <div className="inline-block float-right">
             {filterParam !== "" && <button onClick={() => removeAppliedFilter()} className="btn btn-xs mr-2 btn-active btn-ghost normal-case">{filterParam}<XMarkIcon className="w-4 ml-2"/></button>}
             <div className="dropdown dropdown-bottom dropdown-end"  >
                 <label tabIndex={0} className="btn btn-sm btn-outline" style={{fontSize:20}}><FunnelIcon className="w-5 mr-2"/>Filtrar por Mes</label>
-                <ul style={{"background-color":"rgba(242, 242, 242,0.95)"}} tabIndex={0}   className="dropdown-content menu p-2 text-sm shadow bg-base-100 rounded-box w-52">
+                <ul style={{"background-color":"rgba(242, 242, 242,0.9)"}} tabIndex={0}   className="dropdown-content menu p-2 text-sm shadow bg-base-100 rounded-box w-52">
                     {
                         monthFilters.map((l, k) => {
-                            return  <li key={k}><a onClick={() => showFiltersAndApply(l)} href="/#">{l}</a></li>
+                            return  <li key={k}><a style={aStyle} onClick={() => showFiltersAndApply(l)} href="javascript:;">{l}</a></li>
                         })
                     }
                     <div className="divider mt-0 mb-0"></div>
-                    <li><a onClick={() => removeAppliedFilter()} href="/#">Remover Filtro</a></li>
+                    <li><a style={{...aStyle,"color":"#B9BBFA", "fontWeight":"bolder"}} onClick={() => removeAppliedFilter()} href="javascript:;">Remover Filtro</a></li>
                 </ul>
             </div>
         </div>
